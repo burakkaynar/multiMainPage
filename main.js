a18645df2035f24866a901a0a0153c7c3e0eb502
Burak Kaynar <burakkaynar353@gmail.com>
2024-07-07 01:39:49 +0300

    svg elementlerinin kodu yazıldı

--- main.js
+++ main.js
@@ -16,31 +16,55 @@ const finishBack = document.querySelector("#finish-back")
 const finishBtn = document.querySelector("#finish-btn")
 /* SUMMARY */
 const summary = document.querySelector("#summary")
+/* SVG ELEMENTS */
+const num1h4Element = document.querySelector("#num1 h4")
+const num2h4Element = document.querySelector("#num2 h4")
+const num3h4Element = document.querySelector("#num3 h4")
+const num4h4Element = document.querySelector("#num4 h4")
 
 personelForm.addEventListener("submit", (event) => {
     event.preventDefault()
     personelContent.classList.add("d-none")
     planContent.classList.remove("d-none")
+
+    num1h4Element.style.color = "var(--white)"
+    num1h4Element.style.backgroundColor = "var(--svg-color)"
+
+    num2h4Element.style.color = "var(--black)"
+    num2h4Element.style.backgroundColor = "var(--pastel-blue)"
+    
 })
 
-nextPage(planBtn, planContent, addOnsContent)
-nextPage(addOnBtn, addOnsContent, finishContent)
+nextPage(planBtn, planContent, addOnsContent, num2h4Element, num3h4Element)
+nextPage(addOnBtn, addOnsContent, finishContent, num3h4Element, num4h4Element)
 nextPage(finishBtn, finishContent, summary)
 
-previousPage(planBack, planContent, personelContent)
-previousPage(addOnBack, addOnsContent, planContent)
-previousPage(finishBack, finishContent, addOnsContent)
+previousPage(planBack, planContent, personelContent, num1h4Element, num2h4Element)
+previousPage(addOnBack, addOnsContent, planContent, num2h4Element, num3h4Element)
+previousPage(finishBack, finishContent, addOnsContent, num3h4Element, num4h4Element)
 
-function nextPage(btn, firstContent, secondContent){
+function nextPage(btn, firstContent, secondContent, numElement, numElement2){
     btn.addEventListener("click", () => {
         firstContent.classList.add("d-none")
         secondContent.classList.remove("d-none")
+
+        numElement.style.color = "var(--white)"
+        numElement.style.backgroundColor = "var(--svg-color)"
+
+        numElement2.style.color = "var(--black)"
+        numElement2.style.backgroundColor = "var(--pastel-blue)"
     })
 }
 
-function previousPage(btn, firstContent, secondContent){
+function previousPage(btn, firstContent, secondContent, numElement, numElement2){
     btn.addEventListener("click", () => {
         firstContent.classList.add("d-none")
         secondContent.classList.remove("d-none")
+
+        numElement.style.color = "var(--black)"
+        numElement.style.backgroundColor = "var(--pastel-blue)"
+
+        numElement2.style.color = "var(--white)"
+        numElement2.style.backgroundColor = "var(--svg-color)"
     })
 }
