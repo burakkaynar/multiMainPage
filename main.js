0c94bb691b8be276cf61d6842433de0fee5326b8
Burak Kaynar <burakkaynar353@gmail.com>
2024-07-16 14:53:54 +0300

    tasarım ve kodların neredeyse tamamı yazıldı. hover efektleri ve responsive tasarım yapılacak

--- main.js
+++ main.js
@@ -21,27 +21,42 @@ const num1h4Element = document.querySelector("#num1 h4")
 const num2h4Element = document.querySelector("#num2 h4")
 const num3h4Element = document.querySelector("#num3 h4")
 const num4h4Element = document.querySelector("#num4 h4")
-/* get information */ 
+/* FINISHING UP */ 
 const cardsElement = document.querySelectorAll(".card")
 let planName = document.querySelector("#plan-name h4")
-let priceElement = document.querySelector("#price h3")
+let priceElement = document.querySelector("#price h4")
+const changeButton = document.querySelector("#plan-name p")
+let priceh3Element = document.querySelector("total-price h3")
 /* addons checkbox */
 const extras = document.querySelectorAll(".extra")
 const finishAppendExtras = document.querySelector("#extrasAppend")
 
+cardsElement.forEach(card => {
+    card.addEventListener("click", () => {
+        card.style.backgroundColor = "var(--magnolia)"
+        card.style.border = "1px solid var(--purplish-blue)"
+        const cardValue = card.querySelector("p").getAttribute("value")
+
+        planName.innerHTML = card.querySelector("h3").innerHTML
+        priceElement.innerHTML = card.querySelector("p").innerHTML
+        priceElement.setAttribute("value", cardValue)
+    })
+})
 extras.forEach(extra => {
     const checkbox = extra.querySelector("input")
     const extraName = extra.querySelector("h3").innerHTML
     const extraPrice = extra.querySelector(".extraPrice").innerHTML
+    const extraPriceHTML = extra.querySelector(".extraPrice")
+    const extraValue = extraPriceHTML.getAttribute("value")
 
     let newDivExtra = document.createElement("div")
     newDivExtra.classList.add("extrasInfo")
     newDivExtra.innerHTML = `
     <p>${extraName}</p>
-    <h5>${extraPrice}</h5>
+    <h5 value=${extraValue}>${extraPrice}</h5>
     `
+
     checkbox.addEventListener("change", (event) => {
-        console.log(event.target.checked)
         if(event.target.checked){
             finishAppendExtras.append(newDivExtra)
         }
@@ -49,6 +64,20 @@ extras.forEach(extra => {
             newDivExtra.remove()
         }
     })
+})
+
+addOnBtn.addEventListener("click", () => {
+    let toplam = 0
+    const planPrice = parseInt(document.querySelector("#price h4").getAttribute("value"))
+    toplam += planPrice
+    const extraPrice = document.querySelectorAll(".extrasInfo h5")
+    extraPrice.forEach(price => {
+        const priceValue = parseInt(price.getAttribute("value"))
+        toplam += priceValue
+    })
+    console.log(toplam)
+    const totalPriceElement = document.querySelector("#total-price h3")
+    totalPriceElement.innerHTML = `${toplam}/mo`
 })
 
 personelForm.addEventListener("submit", (event) => {
@@ -70,6 +99,8 @@ nextPage(finishBtn, finishContent, summary)
 previousPage(planBack, planContent, personelContent, num1h4Element, num2h4Element)
 previousPage(addOnBack, addOnsContent, planContent, num2h4Element, num3h4Element)
 previousPage(finishBack, finishContent, addOnsContent, num3h4Element, num4h4Element)
+
+nextPage(changeButton, finishContent, planContent, num4h4Element, num2h4Element)
 
 function nextPage(btn, firstContent, secondContent, numElement, numElement2){
     btn.addEventListener("click", () => {
@@ -97,15 +128,7 @@ function previousPage(btn, firstContent, secondContent, numElement, numElement2)
     })
 }
 
-cardsElement.forEach(card => {
-    card.addEventListener("click", (event) => {
-        card.style.backgroundColor = "var(--magnolia)"
-        card.style.border = "1px solid var(--purplish-blue)"
 
-        planName.innerHTML = card.querySelector("h3").innerHTML
-        priceElement.innerHTML = card.querySelector("p").innerHTML
-    })
-})
 
 document.body.addEventListener("click", (event) => {
     cardsElement.forEach(card => {
